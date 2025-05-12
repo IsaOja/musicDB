18e766aa95b58bcc428b62b85f55ea0433b7cd32
Isac Ojaniemi <39057456+IsaOja@users.noreply.github.com>
2025-05-12 17:55:36 +0200

    removed stuff

--- packages/musicAPI/src/app.js
+++ packages/musicAPI/src/app.js
@@ -5,15 +5,15 @@ const mysqlRoutes = require("./routes/musicdbRoutes");
 const mongoRoutes = require("./routes/mongodbRoutes");
 
 const app = express();
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
-app.use("/api/mysql", mysqlRoutes); // MySQL endpoints
-app.use("/api/mongodb", mongoRoutes); // MongoDB endpoints
+app.use("/api/mysql", mysqlRoutes);
+app.use("/api/mongodb", mongoRoutes);
 
 // Start server
 app.listen(PORT, () => {
